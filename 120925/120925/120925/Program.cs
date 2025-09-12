53424d8b7c07726b4a25b749d58b708c7c42e5c2
vyuka <vyuka@UC332P01-KIV>
2025-09-12 08:39:00 +0200

    -_-

---
+++ 120925/120925/120925/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Tak dobrý den, já se jmenuji:");
+        string? a = Console.ReadLine();
+
+        Console.WriteLine($"Oh hello there {a}");
+        Console.WriteLine("Kdy ses narodil(a)?");
+        int b = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"Aha, takže je ti {getAge(b)} let.");
+    }
+
+    static int getAge(int birth)
+    {
+        return DateTime.Now.Year-birth;
+    }
+}
+
+
+
+
+

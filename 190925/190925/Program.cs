71209a161ddb2e92cae643a0f4bd2834d6d2c060
vyuka <vyuka@UC332P01-KIV>
2025-09-19 08:32:52 +0200

    190925 initial commit

---
+++ 190925/190925/Program.cs
@@ -0,0 +1,6 @@
+﻿int a, b, max;
+Console.WriteLine("Enter two integers:");
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+max = Math.Max(a, b);
+Console.WriteLine($"{max} je větší");

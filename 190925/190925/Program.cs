833043a644235769bd6e66b9e51bcfc74ae42c21
vyuka <vyuka@UC332P01-KIV>
2025-09-19 08:59:02 +0200

    hledání max

--- 190925/190925/Program.cs
+++ 190925/190925/Program.cs
@@ -1,4 +1,4 @@
-﻿int a, b, max;
+﻿/*int a, b, max;
 Console.WriteLine("Enter two integers:");
 a = Convert.ToInt32(Console.ReadLine());
 b = Convert.ToInt32(Console.ReadLine());
@@ -7,3 +7,33 @@ if(a > b)
 else
     max = b;
 Console.WriteLine($"{max} je větší");
+*/
+// NAJDE NEJVETSI ZE 3
+Console.WriteLine("Zadej cisla:");
+
+int[] nums = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+int max = nums[0];
+// O(1)
+if (nums.Length == 3)
+{
+    if (nums[0] >= nums[1] && nums[0] >= nums[2])
+        max = nums[0];
+    else if (nums[1] >= nums[0] && nums[1] >= nums[2])
+        max = nums[1];
+    else
+        max = nums[2];
+}
+else {
+    foreach (int i in nums)
+    {
+        if (i > max)
+            max = i;
+    }
+}
+
+
+    // O(n)
+    
+
+Console.WriteLine($"{max} je nejvetsi cislo");
+
--- 190925/190925/obj/Debug/net8.0/190925.AssemblyInfo.cs
+++ 190925/190925/obj/Debug/net8.0/190925.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("190925")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+624bd2bdf34496a6be7d97fa4d1cebefa0ec79af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4a5f4a37454f4126f3ce2e45c588a08b12df8a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("190925")]
 [assembly: System.Reflection.AssemblyTitleAttribute("190925")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
